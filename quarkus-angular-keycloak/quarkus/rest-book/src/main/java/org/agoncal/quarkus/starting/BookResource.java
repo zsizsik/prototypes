694053dc191ff8b8d47b5912f61eddbede27570c
Zsizsik <zoltan.zsizsik@gmail.com>
2024-12-12 19:48:10 +0100

    Demonstrate public, protected and role based access to endpoints.

--- quarkus-angular-keycloak/quarkus/rest-book/src/main/java/org/agoncal/quarkus/starting/BookResource.java
+++ quarkus-angular-keycloak/quarkus/rest-book/src/main/java/org/agoncal/quarkus/starting/BookResource.java
@@ -1,45 +1,35 @@
 package org.agoncal.quarkus.starting;
 
 import io.quarkus.security.Authenticated;
+import jakarta.annotation.security.RolesAllowed;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-import java.util.List;
-import java.util.Optional;
-
-@Path("/api/books")
-@Produces(MediaType.APPLICATION_JSON)
+@Path("/api")
+@Produces(MediaType.TEXT_PLAIN)
 public class BookResource {
 
-    public record Book(int isbn, String title, String author) {
+    @GET
+    @Path("/public")
+    public String publicEndpoint() {
+        return "This endpoint is public.";
     }
 
-    List<Book> library = List.of(
-            new Book(1, "HP 1", "kathrine "),
-            new Book(2, "HP 2", "kathrine "),
-            new Book(3, "HP 3", "kathrine ")
-    );
-
     @GET
-    @Path("/count")
-    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/protected")
     @Authenticated
-    public int count() {
-        return library.size();
+    public String protectedEndpoint() {
+        return "This endpoint is only available for authenticated users.";
     }
 
     @GET
-    public List<Book> getAllBooks() {
-        return library;
-    }
-
-    @GET
-    @Path("/{id}")
-    public Optional<Book> getBook(@PathParam("id") int id) {
-        return library.stream().filter(b -> b.isbn() == id).findFirst();
+    @Path("/protected/admin")
+    @Authenticated
+    @RolesAllowed("ADMIN")
+    public String adminEndpoint() {
+        return "This endpoint is only available for authenticated users having admin role.";
     }
 
 }
