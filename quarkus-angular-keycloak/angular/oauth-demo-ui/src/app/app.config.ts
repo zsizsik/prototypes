471e6e3503f8f40aeedb7abeea9c430041df0b3c
Zoltán Zsizsik <geza-zoltan.zsizsik@t-systems.com>
2024-12-14 11:56:21 +0100

    material

--- quarkus-angular-keycloak/angular/oauth-demo-ui/src/app/app.config.ts
+++ quarkus-angular-keycloak/angular/oauth-demo-ui/src/app/app.config.ts
@@ -5,7 +5,8 @@ import { routes } from './app.routes';
 import { authConfig } from './auth/auth.config';
 import { provideAuth } from 'angular-auth-oidc-client';
 import {provideHttpClient} from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAuth(authConfig), provideHttpClient()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAuth(authConfig), provideHttpClient(), provideAnimationsAsync(), provideAnimationsAsync()]
 };
